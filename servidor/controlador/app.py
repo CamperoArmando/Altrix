d39da053aae959d9ed3cbc527c402c0574bd6791
CamperoArmando <126182473+CamperoArmando@users.noreply.github.com>
2026-07-20 09:46:18 -0600

    error en app.py

--- servidor/controlador/app.py
+++ servidor/controlador/app.py
@@ -22,12 +22,7 @@ from controlador.producto_controlador import producto_bp
 app.register_blueprint(producto_bp)
 
 
-def _sembrar_roles():
-    """Crea los roles base (Administrador, Vendedor) si no existen."""
-    for nombre in ("Administrador", "Vendedor"):
-        if not Rol.query.filter_by(nombre=nombre).first():
-            db.session.add(Rol(nombre=nombre))
-    db.session.commit()
+
 
 
 with app.app_context():
